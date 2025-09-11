a1ac6be8118887a4df03c45644cacb55d497f53e
Mehrad <2523528+xehrad@users.noreply.github.com>
2025-09-11 16:12:29 +0330

    [fix] The "option" bug is now being handled

--- form-json.js
+++ form-json.js
@@ -1,22 +1,22 @@
-(function() {
+(function () {
   let api
   const _ConfigIgnoreDeepKey_ = 'ignore-deep-key'
   const _FlagObject_ = 'obj'
-  const _FlagArray_  = 'arr'
-  const _FlagValue_  = 'val'
+  const _FlagArray_ = 'arr'
+  const _FlagValue_ = 'val'
 
   htmx.defineExtension('form-json', {
-    init: function(apiRef) {
+    init: function (apiRef) {
       api = apiRef
     },
 
-    onEvent: function(name, evt) {
+    onEvent: function (name, evt) {
       if (name === 'htmx:configRequest') {
         evt.detail.headers['Content-Type'] = 'application/json'
       }
     },
 
-    encodeParameters: function(xhr, parameters, elt) {
+    encodeParameters: function (xhr, parameters, elt) {
       let object = {}
       xhr.overrideMimeType('text/json')
 
@@ -35,11 +35,11 @@
 
       // FormData encodes values as strings, restore hx-vals/hx-vars with their initial types
       const vals = api.getExpressionVars(elt)
-      Object.keys(object).forEach(function(key) {
+      Object.keys(object).forEach(function (key) {
         object[key] = Object.hasOwn(vals, key) ? vals[key] : object[key]
       })
 
-      if(!api.hasAttribute(elt, _ConfigIgnoreDeepKey_)){
+      if (!api.hasAttribute(elt, _ConfigIgnoreDeepKey_)) {
         const flagMap = getFlagMap(object)
         object = buildNestedObject(flagMap, object)
       }
@@ -51,7 +51,10 @@
     if (inputType == 'number' || inputType == 'range') {
       return Array.isArray(value) ? value.map(Number) : Number(value)
     } else if (inputType === 'checkbox') {
-      return value || true
+      if (input.defaultValue) {
+        return input.defaultChecked ? input.defaultValue : value
+      }
+      return true
     }
     return value
   }
@@ -64,55 +67,55 @@
 
   function getFlagMap(map) {
     const flagMap = {}
-    
-    for (const key in map) {
-        const parts = splitKey(key)
-        parts.forEach((part, i) => {
-            const path = parts.slice(0, i+1).join('.')
-            const isLastPart = i === parts.length - 1
-            const nextIsNumeric = !isLastPart && !isNaN(Number(parts[i + 1]))
 
-            if (isLastPart) {
-                    flagMap[path]= _FlagValue_
-            } else {
-                if (!flagMap.hasOwnProperty(path)) {
-                    flagMap[path] = nextIsNumeric ? _FlagArray_ : _FlagObject_
-                }else if(flagMap[path]===_FlagValue_ || !nextIsNumeric){
-                    flagMap[path] = _FlagObject_
-                }
-            }
-        })
+    for (const key in map) {
+      const parts = splitKey(key)
+      parts.forEach((part, i) => {
+        const path = parts.slice(0, i + 1).join('.')
+        const isLastPart = i === parts.length - 1
+        const nextIsNumeric = !isLastPart && !isNaN(Number(parts[i + 1]))
+
+        if (isLastPart) {
+          flagMap[path] = _FlagValue_
+        } else {
+          if (!flagMap.hasOwnProperty(path)) {
+            flagMap[path] = nextIsNumeric ? _FlagArray_ : _FlagObject_
+          } else if (flagMap[path] === _FlagValue_ || !nextIsNumeric) {
+            flagMap[path] = _FlagObject_
+          }
+        }
+      })
     }
 
     return flagMap
   }
 
   function buildNestedObject(flagMap, map) {
-      const out = {}
+    const out = {}
 
-      for (const key in map) {
-        const parts = splitKey(key)
-        let current = out
-        parts.forEach((part, i) => {
-            const path = parts.slice(0, i + 1).join('.')
-            const isLastPart = i === parts.length - 1 
+    for (const key in map) {
+      const parts = splitKey(key)
+      let current = out
+      parts.forEach((part, i) => {
+        const path = parts.slice(0, i + 1).join('.')
+        const isLastPart = i === parts.length - 1
 
-            if (isLastPart){
-                if (flagMap[path] === _FlagObject_){
-                    current[part] = { '': map[key] }
-                } else if (part === '-1'){
-                  const val = map[key]
-                  Array.isArray(val) ? current.push(...val) : current.push(val)
-                } else {
-                  current[part] = map[key]
-                }    
-            } else if(!current.hasOwnProperty(part)) {
-                current[part] = flagMap[path] === _FlagArray_ ? [] : {}
-            }
-            
-            current = current[part]
-        })
-      }
-      return out
+        if (isLastPart) {
+          if (flagMap[path] === _FlagObject_) {
+            current[part] = { '': map[key] }
+          } else if (part === '-1') {
+            const val = map[key]
+            Array.isArray(val) ? current.push(...val) : current.push(val)
+          } else {
+            current[part] = map[key]
+          }
+        } else if (!current.hasOwnProperty(part)) {
+          current[part] = flagMap[path] === _FlagArray_ ? [] : {}
+        }
+
+        current = current[part]
+      })
+    }
+    return out
   }
 })()
